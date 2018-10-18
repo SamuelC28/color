c506869ef76797a14d0dd12c182bc49ce62e5b4d
SamuelC28 <samuel@noukod.com>
2018-10-18 10:42:57 -0400

    kot kob petro karibe a

---
+++ color.rb
@@ -0,0 +1,20 @@
+class String
+    # String.include AllOfMyColors
+    def bg_red; "\e[41m#{self}\e[0m" end
+    def bg_blue; "\e[44m#{self}\e[0m" end
+    def white; "\e[38m#{self}\e[0m" end
+    def green; "\e[32m#{self}\e[0m" end 
+end
+
+class MyLogColor
+    #using String
+    #include String
+    def haitis_flag(bleu_part, message, red_part)
+#         
+         puts "%s %s %s" % [bleu_part.bg_blue, message.white.bg_blue, red_part.bg_red]
+    end                                               
+
+end
+
+MyLogColor.new.haitis_flag("                              \n                           \n                              ","\n     Kot Kob PetroKaribe a? ", "\n                             \n                             ")
+# end
